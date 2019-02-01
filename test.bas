578a1a05fdc75631b5b28a8c7d17d20de959947b
jonlindg <ejonathanlindgren@gmail.com>
2019-02-01 15:25:05 +0100

    Add files via upload

--- test.bas
+++ test.bas
@@ -1,30 +1,49 @@
 
-Function Initialize() Uint64
-10  STORE("number",10)
-11  STORE("string1","hello")
-12  STORE("string2","world")
-20 RETURN 0 
-End Function 
+	Function Initialize() Uint64
+    10  STORE("message_count",1)
+	20  STORE("message0", "Hello world!")
+	30 RETURN 0 
+	End Function 
+	
+    Function Write(text String) Uint64
+    5   IF EXISTS(SIGNER())==0 THEN GOTO 60
+    10  DIM current_message_count as Uint64
+    20  LET current_message_count = LOAD("message_count")
+    30  STORE("message"+current_message_count,LOAD(SIGNER())+": "+text)
+    40  STORE("message_count",current_message_count+1)
+    50  RETURN 0
+    60  RETURN 1
+    END FUNCTION
 
-Function ChangeValue(new_val1 Uint64,new_val2 Uint64) Uint64
-30  STORE("number",new_val1*new_val2)
-50  RETURN 0
-END FUNCTION
+    Function Register(name String) Uint64
+    5   IF EXISTS(SIGNER())==1 THEN GOTO 40
+    6   IF EXISTS(name)==1 THEN GOTO 50
+    7   DIM username,signer as String
+    8   LET username = name
+    9   LET signer = SIGNER()
+    10  STORE(SIGNER(),name)
+    20  STORE(name,SIGNER())
+    25  PRINTF "%s is now associated to username %s" signer username
+    30  RETURN 0
+    40  PRINTF "This address is already registered"
+    45  RETURN 1
+    50  PRINTF "This username is already taken"
+    55  RETURN 1
+    END FUNCTION
 
-Function DepositAndChangeValue(new_val1 Uint64,value Uint64,new_val2 Uint64) Uint64
-30  STORE("number",new_val1*new_val2)
-50  RETURN 0
-END FUNCTION
+    Function Get_number_of_messages() Uint64
+    5   DIM message_count as Uint64
+    6   LET message_count = LOAD("message_count")
+    10  PRINTF "Number of messages: %d" message_count
+    20  RETURN 0
+    END FUNCTION
 
-Function DepositAndChangeValueAndStrings(new_val1 Uint64,value Uint64,new_val2 Uint64,new_str1 String, new_str2 String) Uint64
-30  STORE("number",new_val1*new_val2)
-35  STORE("string1",new_str1)
-40  STORE("string2",new_str2)
-50  RETURN 0
-END FUNCTION
+    FUNCTION Print_message(n Uint64) Uint64
+    5   DIM message as String
+    6   LET message = LOAD("message"+n)
+    10  PRINTF "%s" message
+    20  RETURN 0
+    END FUNCTION
 
-Function Withdraw(new_val1 Uint64,amount Uint64,new_val2 Uint64) Uint64
-30  STORE("number",new_val1*new_val2)
-40  SEND_DERO_TO_ADDRESS(SIGNER(),amount)
-50  RETURN 0
-END FUNCTION
+
+	
--- test.py
+++ test.py
@@ -1,29 +1,43 @@
+import time
+import json
+
 #import the relevant classes from pydero
 from pydero import Contract,Connection
 
 #specify the scid
-scid='d1668fce0d0e9c3191b7b8d174ddde9269ffbfb6f0bf85b07b3747411c541454'
+scid='2849f5c32d385b8b972e53dde0abd5b6458f8eb9367f1215e7e95b819f739592'
 
-#create a Connection instance, specifying IP address and port of the running wallet and daemon. Note that the wallet must be running with the --rpc-server flag
+#create a Connection instance, specifying IP address and port of the running wallet. Note that the wallet must be running with the --rpc-server flag
 conn = Connection('http://127.0.0.1:30309','http://127.0.0.1:30306')
 
 #specify abi compatible with the smart contract source code
-abi = {"ChangeValue":{"new_val1":"Uint64","new_val2":"Uint64"},"DepositAndChangeValue":{"new_val1":"Uint64","value":"Uint64","new_val2":"Uint64"},"DepositAndChangeValueAndStrings":{"new_val1":"Uint64","value":"Uint64","new_val2":"Uint64","new_str1":"String","new_str2":"String"},"Withdraw":{"new_val1":"Uint64","amount":"Uint64","new_val2":"Uint64"}}
+abi = {"Print":{},"ChangeValue":{"new_val":"Uint64"}}
 
 #create an instance of the Contract class
 contract = Contract(abi,connection=conn,scid=scid)
 
-print(contract.get_attribute('number'))
-print(contract.get_attribute('string1'))
-print(contract.get_attribute('string2'))
-
+resp = contract.get_attribute('number')
+print(resp)
 #now you can call functions on the contract
-#print(contract.ChangeValue(9,11))
-#print(contract.DepositAndChangeValue(10,3100000000000,22))
-#print(contract.DepositAndChangeValueAndStrings(10,3100000000000,20,"ASDF","QWERTY"))
-#print(contract.Withdraw(5,5000000000000,7))
+#print(contract.Print.args)
+#print(contract.ChangeValue.args)
+#contract.Print()
+#time.sleep(10)
+#contract.ChangeValue(999)
+#time.sleep(10)
+#contract.Print()
+#time.sleep(10)
+#contract.ChangeValue(1098000)
 
 
 
 
 
+    
+
+#curl -X POST http://127.0.0.1:30309/json_rpc -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"mixin":5,"get_tx_key": true , "sc_tx":{"entrypoint":"Print","scid":"2849f5c32d385b8b972e53dde0abd5b6458f8eb9367f1215e7e95b819f739592", "params":{} } }}'
+
+#curl -X POST http://127.0.0.1:30309/json_rpc -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":"0","method":"transfer_split","params":{"mixin":5,"get_tx_key": true , "sc_tx":{"entrypoint":"ChangeValue","scid":"2849f5c32d385b8b972e53dde0abd5b6458f8eb9367f1215e7e95b819f739592", "params":{"new_val":"763"} } }}'
+            
+
+#    curl -X POST http://127.0.0.1:30306/gettransactions -d '{"txs_hashes":["2849f5c32d385b8b972e53dde0abd5b6458f8eb9367f1215e7e95b819f739592"], "sc_keys":["number"]}' -H 'Content-Type: application/json
